d6d0bafa302c641f57fb49b3459bb4ea09937c7d
moken <moken@LAPTOP-IH3NAIAC>
2024-06-14 03:34:14 +0300

    designing a new template for home page.

---
+++ Design prototyping/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Smooth scrolling for links
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+});
